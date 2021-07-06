b1a5c0f47ee9d4956616b6accbfe8d12bd7c710d
Leon Fan <leonkind@gmail.com>
2021-07-05 20:42:45 -0500

    commit tree d3958ef08550ca1f5030265edc430a3e36819c5c

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,24 @@
+class PlantsController < ApplicationController
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+
+    def show
+        plant = Plant.find(params[:id])
+        if plant
+            render json: plant
+        else
+            render json: {error: '404 plant not found'}, status: :not_found
+        end
+    end
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+end
